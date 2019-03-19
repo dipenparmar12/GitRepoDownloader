3a8938b757816647dd4956096a4322db5b91e989
DIpends <dipensavji@gmail.com>
2019-03-19 22:21:36 +0530

    GitUser Details Data Loaded successfuly in GitUserDetail_UI Activity layout

--- app/src/main/java/com/mscit/gitgubusers/GitApi.java
+++ app/src/main/java/com/mscit/gitgubusers/GitApi.java
@@ -1,6 +1,5 @@
 package com.mscit.gitgubusers;
 
-import java.net.URL;
 import java.util.ArrayList;
 
 import retrofit2.Call;
@@ -9,13 +8,12 @@ import retrofit2.http.Path;
 
 public interface GitUserApi {
 
-
     public final String URL = "https://api.github.com/users/";
-    @GET("{gitUser}/repos")
-    Call<ArrayList<GitUserDetail>>  GitUserDetail(@Path("gitUser") String gitUser);
 
-//    public final String URL = "http://127.0.0.1/";
-//    @GET("api.php")
-//    Call<ArrayList<GitUserDetail>>  GitUserDetail();
+    @GET("{gitUser}/repos")
+    Call<ArrayList<GitRepoDetail>>  GitUserDetail(@Path("gitUser") String gitUser);
+
+    @GET("{gitUser}/repos")
+    Call<ArrayList<GitUserDetail>>  GitRepoDetail(@Path("gitUser") String gitUser);
 
 }
---
+++ app/src/main/java/com/mscit/gitgubusers/GitRepoDetail.java
@@ -0,0 +1,167 @@
+package com.mscit.gitgubusers;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.security.acl.Owner;
+
+public class GitUserDetail {
+    private Integer id;
+    private  String name,full_name,html_url,description,url,languages_url,contributors_url,created_at,updated_at,git_url,clone_url,svn_url,homepage,size,watchers_count,forks_count,archived,forks,watchers,default_branch;
+
+    public GitUserDetail(Integer id, String name, String full_name, String html_url, String description, String url, String languages_url, String contributors_url, String created_at, String updated_at, String git_url, String clone_url, String svn_url, String homepage, String size, String watchers_count, String forks_count, String archived, String forks, String watchers, String default_branch) {
+        this.id = id;
+        this.name = name;
+        this.full_name = full_name;
+        this.html_url = html_url;
+        this.description = description;
+        this.url = url;
+        this.languages_url = languages_url;
+        this.contributors_url = contributors_url;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+        this.git_url = git_url;
+        this.clone_url = clone_url;
+        this.svn_url = svn_url;
+        this.homepage = homepage;
+        this.size = size;
+        this.watchers_count = watchers_count;
+        this.forks_count = forks_count;
+        this.archived = archived;
+        this.forks = forks;
+        this.watchers = watchers;
+        this.default_branch = default_branch;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFull_name() {
+        return full_name;
+    }
+
+    public String getHtml_url() {
+        return html_url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getLanguages_url() {
+        return languages_url;
+    }
+
+    public String getContributors_url() {
+        return contributors_url;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public String getUpdated_at() {
+        return updated_at;
+    }
+
+    public String getGit_url() {
+        return git_url;
+    }
+
+    public String getClone_url() {
+        return clone_url;
+    }
+
+    public String getSvn_url() {
+        return svn_url;
+    }
+
+    public String getHomepage() {
+        return homepage;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getWatchers_count() {
+        return watchers_count;
+    }
+
+    public String getForks_count() {
+        return forks_count;
+    }
+
+    public String getArchived() {
+        return archived;
+    }
+
+    public String getForks() {
+        return forks;
+    }
+
+    public String getWatchers() {
+        return watchers;
+    }
+
+    public String getDefault_branch() {
+        return default_branch;
+    }
+
+}
+
+
+//public class GitUserDetail {
+//
+//    private Integer id;
+//    private String name,full_name,homepage,description,message,documentation_url;
+//    public GitUserDetail(Integer id, String name, String full_name, String homepage, String description) {
+//        this.id = id;
+//        this.name = name;
+//        this.full_name = full_name;
+//        this.homepage = homepage;
+//        this.description = description;
+//    }
+//
+//
+//
+//    public Integer getId() {
+//        return id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public String getFull_name() {
+//        return full_name;
+//    }
+//
+//    public String getHomepage() {
+//        return homepage;
+//    }
+//
+//    public String getDescription() {
+//        return description;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "GitUserDetail{" +
+//                "id=" + id +
+//                ", name='" + name + '\'' +
+//                ", full_name='" + full_name + '\'' +
+//                '}';
+//    }
+//
+//}
+//
